8ef637910a3e36bcffb63c486a7eb86337dc4efc
Jo <jo@local>
2020-09-07 11:46:44 +0500

    first step

---
+++ step_01.py
@@ -0,0 +1,10 @@
+# var string: message;  # pascal, c, c++, c#, Java - static typing
+# message: = 5; -> error
+# dynamic typing: python, js, php -> development speed up
+# dynamic typing -> danger ->
+
+message = 'Hello, world'
+print(message, type(message))
+
+message = 5
+print(message, type(message))
